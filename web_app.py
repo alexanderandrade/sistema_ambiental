9ca7956dd17b0691a74b8c2973d16cbffc5c7441
Jessner <macbook@MacBook-Pro-de-macbook.local>
2019-01-21 14:29:56 -0500

    Se agregan las funcionalidades al api de consultar perfil y tendencia

--- web_app.py
+++ web_app.py
@@ -2,10 +2,15 @@ from flask import Flask
 import requests
 
 app= Flask(__name__)
+@app.route('/home/<nombre>')
+def home(nombre):
+    info = requests.get('http://0.0.0.0:5000/'+nombre)
+    return info.text
+
 @app.route('/consultar/<nombre>')
 def consultar(nombre):
-    info = requests.get('http://0.0.0.0:5000/consultar/'+nombre)
-    return info.text
+    perfil= requests.get('http://127.0.0.1:5000/consultar/'+nombre)
+    return perfil.text
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=80)
